17c857ddae76620d7ffcc0625a5b8b695d93f359
Amirhossein Saberi (امیرحسین صابری) <signor.stylish@gmail.com>
2019-05-12 17:37:27 +0430

    Update Playstation.php

--- config/Playstation.php
+++ config/Playstation.php
@@ -1,1 +1,30 @@
+<?php
 
+return [
+    'parser' => [
+        'name'          => 'PlayStation',
+        'enabled'       => true,
+        'sender_map'    => [
+            '/no-reply@sie.sony.com/',
+            '/rtabuse-general@shatel.ir/',
+        ],
+        'body_map'      => [
+            //
+        ],
+        'aliases'       => [
+        ],
+    ],
+
+    'feeds' => [
+        'login-attack' => [
+            'class'     => 'HACK_ATTACK',
+            'type'      => 'ABUSE',
+            'enabled'   => true,
+            'fields'    => [
+                'Report-Type',
+                'Source',
+                'Date',
+            ],
+        ],
+    ],
+];
